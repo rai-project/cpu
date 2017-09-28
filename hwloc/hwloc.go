da600979482c4ca786ffd4bb7475031e73f172c9
Abdul Dakkak <abduld@wolfram.com>
2017-09-28 05:04:26 -0500

    add cpu tools

---
+++ hwloc/hwloc.go
@@ -0,0 +1,152 @@
+// Copyright (c) 2016 Andrey Sibiryov <me@kobology.ru>
+// Copyright (c) 2016 Other contributors as noted in the AUTHORS file.
+//
+// This file is part of Tesson.
+//
+// Tesson is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// Tesson is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package hwloc
+
+// #include <hwloc.h>
+// #cgo CFLAGS: -Wno-deprecated-declarations
+// #cgo LDFLAGS: -lhwloc
+import "C"
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var (
+	errInternalHwlocError = errors.New("internal hwloc error")
+)
+
+// Topology represents the hardware layout of a machine.
+type Topology interface {
+	N() int
+	Distribute(n int, opts DistributeOptions) ([]Unit, error)
+}
+
+// Unit represents a unit of allocation (e.g. cpuset).
+type Unit interface {
+	String() string
+	Weight() int
+}
+
+// DistributeOptions specifies options for Distribute.
+type DistributeOptions struct {
+	Granularity Granularity
+}
+
+// Granularity specifies distribution granularity.
+type Granularity uint
+
+// ParseGranularity parses granularity strings.
+func ParseGranularity(g string) (Granularity, error) {
+	switch strings.ToLower(g) {
+	case "node":
+		return NodeGranularity, nil
+	case "core":
+		return CoreGranularity, nil
+	}
+
+	return 0, fmt.Errorf("error parsing '%s'", g)
+}
+
+// A list of supported distribution granularities.
+const (
+	NodeGranularity Granularity = iota
+	CoreGranularity
+)
+
+// Implementation
+
+// NewHwlocTopology constructs a Topology for the local machine,
+// implemented in terms of libhwloc.
+func NewHwlocTopology() (Topology, error) {
+	t := &hwloc{}
+
+	var r C.int
+
+	r = C.hwloc_topology_init(&t.ptr)
+
+	if r != 0 {
+		return nil, errInternalHwlocError
+	}
+
+	r = C.hwloc_topology_load(t.ptr)
+
+	if r != 0 {
+		return nil, errInternalHwlocError
+	}
+
+	return t, nil
+}
+
+type hwloc struct {
+	ptr C.hwloc_topology_t
+}
+
+func (t *hwloc) N() int {
+	return int(C.hwloc_get_nbobjs_by_type(t.ptr, C.HWLOC_OBJ_CORE))
+}
+
+type unit struct {
+	c C.hwloc_cpuset_t
+}
+
+func (u unit) String() string {
+	b := [64]C.char{}
+	n := C.hwloc_bitmap_list_snprintf(&b[0], C.size_t(len(b)), u.c)
+
+	return C.GoStringN(&b[0], n)
+}
+
+func (u unit) Weight() int {
+	return int(C.hwloc_bitmap_weight(u.c))
+}
+
+func (t *hwloc) Distribute(
+	n int, opts DistributeOptions) ([]Unit, error) {
+
+	var (
+		roots = C.hwloc_get_root_obj(t.ptr)
+		depth = C.hwloc_get_type_or_below_depth(
+			t.ptr, opts.Granularity.build())
+		l = make([]C.hwloc_cpuset_t, n)
+	)
+
+	C.hwloc_distribute(
+		t.ptr, roots, &l[0], C.uint(len(l)), C.uint(depth))
+
+	r := make([]Unit, n)
+
+	for i, c := range l {
+		r[i] = &unit{c: c}
+	}
+
+	return r, nil
+}
+
+func (g Granularity) build() C.hwloc_obj_type_t {
+	switch g {
+	case NodeGranularity:
+		return C.HWLOC_OBJ_NODE
+	case CoreGranularity:
+		return C.HWLOC_OBJ_CORE
+	}
+
+	panic("hwloc: granularity type not supported")
+}
---
+++ numa/numa.go
@@ -0,0 +1,158 @@
+// from https://github.com/cheyang/numa-utils
+package numa
+
+import (
+	"errors"
+	"fmt"
+)
+
+// #cgo LDFLAGS: -lnuma
+// #include <numa.h>
+import "C"
+
+var NumaNotAvailable = errors.New("No NUMA support available on this system")
+
+/* Is Numa available? */
+func IsNumaAvailable() int {
+	return int(C.numa_available())
+}
+
+/* Get max available node */
+func MaxNode() int         { return int(C.numa_max_node()) }
+func MaxPossibleNode() int { return int(C.numa_max_possible_node()) }
+
+func NumConfiguredCPUs() int { return int(C.numa_num_configured_cpus()) }
+
+/**
+* Get Memory of the Numa node
+**/
+func MemoryOfNode(node int) (inAll, free uint64) {
+	cFree := C.longlong(0)
+	cInAll := C.numa_node_size64(C.int(node), &cFree)
+	return uint64(cInAll), uint64(cFree)
+}
+
+func MemInMB(mem uint64) uint64 {
+	return mem >> 20
+}
+
+/**
+* Get Numa Nodes
+**/
+func Nodes() (nodes []int, err error) {
+
+	if IsNumaAvailable() < 0 {
+		return nodes, NumaNotAvailable
+	}
+
+	maxnode := int(C.numa_max_node())
+	for i := 0; i <= maxnode; i++ {
+		if C.numa_bitmask_isbitset(C.numa_nodes_ptr, C.uint(i)) > 0 {
+			nodes = append(nodes, i)
+		}
+	}
+
+	return nodes, nil
+}
+
+/**
+* Get CPU slice from the specified Node
+**/
+func CPUsOfNode(node int) (cpus []int, err error) {
+
+	if IsNumaAvailable() < 0 {
+		return cpus, NumaNotAvailable
+	}
+
+	mask := C.numa_allocate_cpumask()
+	defer C.numa_free_cpumask(mask)
+
+	rc := C.numa_node_to_cpus(C.int(node), mask)
+	maxCpus := NumConfiguredCPUs()
+	if rc >= 0 {
+		for i := 0; i < maxCpus; i++ {
+			if C.numa_bitmask_isbitset(mask, C.uint(i)) > 0 {
+				cpus = append(cpus, i)
+			}
+		}
+	} else {
+		return cpus, NumaNotAvailable
+	}
+
+	return cpus, nil
+
+}
+
+func PrintDistance() {
+
+	maxnode := MaxNode()
+	fst := 0
+
+	for i := 0; i < maxnode; i++ {
+		if C.numa_bitmask_isbitset(C.numa_nodes_ptr, C.uint(i)) > 0 {
+			fst = i
+			break
+		}
+	}
+
+	if C.numa_distance(C.int(maxnode), C.int(fst)) == 0 {
+		fmt.Println("No distance information available.")
+		return
+	}
+
+	fmt.Printf("node distances:\n")
+	fmt.Printf("node ")
+	for i := 0; i <= maxnode; i++ {
+		if int(C.numa_bitmask_isbitset(C.numa_nodes_ptr, C.uint(i))) > 0 {
+
+			fmt.Printf("% 3d ", i)
+		}
+	}
+	fmt.Printf("\n")
+	for i := 0; i <= maxnode; i++ {
+		if int(C.numa_bitmask_isbitset(C.numa_nodes_ptr, C.uint(i))) == 0 {
+			continue
+		}
+		fmt.Printf("% 3d: ", i)
+		for k := 0; k <= maxnode; k++ {
+			if C.numa_bitmask_isbitset(C.numa_nodes_ptr, C.uint(i)) > 0 &&
+				C.numa_bitmask_isbitset(C.numa_nodes_ptr, C.uint(k)) > 0 {
+				fmt.Printf("% 3d ", C.numa_distance(C.int(i), C.int(k)))
+			}
+		}
+		fmt.Printf("\n")
+	}
+}
+
+type Distance struct {
+	Start  uint32
+	End    uint32
+	Length uint32
+}
+
+func GetDistances() (distances []Distance) {
+	maxnode := MaxNode()
+
+	for i := 0; i <= maxnode; i++ {
+		if int(C.numa_bitmask_isbitset(C.numa_nodes_ptr, C.uint(i))) == 0 {
+			continue
+		}
+
+		for k := 0; k <= maxnode; k++ {
+			if C.numa_bitmask_isbitset(C.numa_nodes_ptr, C.uint(i)) > 0 &&
+				C.numa_bitmask_isbitset(C.numa_nodes_ptr, C.uint(k)) > 0 {
+
+				distance := Distance{
+					Start:  uint32(i),
+					End:    uint32(k),
+					Length: uint32(C.numa_distance(C.int(i), C.int(k))),
+				}
+
+				distances = append(distances, distance)
+				fmt.Printf("append distance %v\n", distance)
+			}
+		}
+	}
+
+	return
+}
